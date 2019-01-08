ea588d8aac1c0bb9f9b6286c03d45eb7fefdd217
Ravish23 <ravishshafqat@gmail.com>
2019-01-09 05:46:04 +0800

    version 1

---
+++ src/app/data_parameters_model.ts
@@ -0,0 +1,7 @@
+export class DataParameters {
+  nodes_number: number;
+  node_labels: string[];
+  relationships_number: number;
+  relationship_types: string[];
+  distribution_relationships: string;
+}
